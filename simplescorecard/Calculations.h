9a775bf2ac78340ac181d950adaa3534cf80ddfc
initWit <figueras.robert@gmail.com>
2014-08-05 14:48:29 -0500

    Refactored ScoreCard VC to use new Calcualtor Class

---
+++ simplescorecard/Calculations.h
@@ -0,0 +1,13 @@
+//
+//  Calculations.h
+//  SimpleScoreCard
+//
+//  Created by Robert Figueras on 8/5/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Calculations : NSObject
+
+@end
---
+++ simplescorecard/Calculations.m
@@ -0,0 +1,13 @@
+//
+//  Calculations.m
+//  SimpleScoreCard
+//
+//  Created by Robert Figueras on 8/5/14.
+//
+//
+
+#import "Calculations.h"
+
+@implementation Calculations
+
+@end
