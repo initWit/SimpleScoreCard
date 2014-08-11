7a6473e9dd21ea6a4e736ab873b5c24fad54d5fd
initWit <figueras.robert@gmail.com>
2014-08-11 10:09:36 -0500

    moved files

---
+++ simplescorecard/MyCollectionViewCell.m
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.m
+//  SimpleScoreCard
+//
+//  Created by Robert Figueras on 7/30/14.
+//
+//
+
+#import "MyCollectionViewCell.h"
+
+@implementation MyCollectionViewCell
+
+
+@end
